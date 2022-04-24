f86c2dfb0eb27afd5340341354c603e8fc578c6c
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-24 20:50:41 +0300

    Feature: constructor added and arguments passed

---
+++ User.py
@@ -0,0 +1,18 @@
+class User:
+    '''
+    Class that generates new instances of the Users
+    '''
+    pass
+
+def __init__(self, first_name, last_name, user_name,phone_number, email,):
+    '''
+    The properties needed by the application for the user will require, their names, 
+    their usernames, phone numbers and emails
+
+    Args: 
+    first_name: New user first name.
+    last_name: new user first name.
+    user_name: new user user name.
+    phone_number: new user phone number.
+    email: new contact email address. 
+    '''
